76967a0ca451652a36a63337f7841d8992c5a89d
lixionglue@vip.qq.com <lixionglue@vip.qq.com>
2020-12-08 20:53:18 +0800

    Chat implements UKTableViewDataSrouce, UKTableViewDelegate

--- assets/script/scene/Chat.ts
+++ assets/script/scene/Chat.ts
@@ -1,9 +1,16 @@
 import UKTableView from "../UKTableView/UKTableView";
+import { UKTableViewDataSrouce } from "../UKTableView/UKTableViewDataSource";
+import { UKTableViewDelegate } from "../UKTableView/UKTableViewDelegate";
+import { ChatModel } from "./chat/model/ChatModel";
 
 const {ccclass, property, menu} = cc._decorator;
 
+enum EChatCellType {
+    text = 'text',
+}
+
 @ccclass
-export default class Chat extends cc.Component {
+export default class Chat extends cc.Component implements UKTableViewDataSrouce, UKTableViewDelegate {
     @property(UKTableView)
     private tableView: UKTableView = null;
 
@@ -13,6 +20,14 @@ export default class Chat extends cc.Component {
     @property(cc.Prefab)
     private preChatText: cc.Prefab = null;
 
+    private models: ChatModel[] = [];
+
+    onLoad() {
+        this.tableView.registe(this.preChatText, EChatCellType.text);
+        this.tableView.delegate = this;
+        this.tableView.dataSource = this;
+    }
+
     clickBack() {
         cc.director.loadScene('Test');
     }
@@ -20,4 +35,16 @@ export default class Chat extends cc.Component {
     onTextEditEnd() {
 
     }
-}
+
+    // MARK: UKTableViewDataSrouce
+    cellAtIndex(index: number) {
+        const cell = this.tableView.dequeueReusableCell(EChatCellType.text);
+        
+        return cell;
+    }
+
+    // MARK: UKTableViewDelegate
+    estimateSizeAtIndex(index: number) {
+        return 100;
+    }
+}   
---
+++ assets/script/scene/chat/model/ChatModel.ts
@@ -0,0 +1,7 @@
+export class ChatModel {
+    userId: number;
+    time: number;
+
+    /** 聊天的文本 */
+    text?: string;
+}
